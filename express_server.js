be62eee954295d58c2dd89f2d3467ef06096d664
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-06-13 15:13:43 -0400

    created urls_shows.ejs and added the header partial inside urls_index.ejs and urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -14,16 +14,24 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
+  res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
