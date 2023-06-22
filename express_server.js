7f0dae2f9ae36f8e3eabe20d33cd2bf021f6c871
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-06-22 14:56:17 -0400

    removed unnecessary console log

--- express_server.js
+++ express_server.js
@@ -220,7 +220,6 @@ app.post("/login", (req, res) => {
 app.post("/logout", (req, res) => {
   // res.clearCookie("user_id"); //Clear the user_id cookie
   req.session = null;
-  console.log("Hello");
   res.redirect("/login"); // Redirect the user to the login page
 });
 
